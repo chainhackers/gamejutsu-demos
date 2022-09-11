90aec559712e0ccbbaf3973ec95527d26edf702a
poludnev <poludnev.roman@gmail.com>
2022-09-11 23:44:14 +0300

    Updated messaging

--- context/WalltetContext.tsx
+++ context/WalltetContext.tsx
@@ -1,15 +1,15 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import Web3Modal, { IProviderOptions, providers } from 'web3modal';
-import WalletConnectProvider from '@walletconnect/web3-provider'
+import WalletConnectProvider from '@walletconnect/web3-provider';
 import { ethers, Signer } from 'ethers';
 interface WalletContextProps {
-    signer: Signer | undefined;
-    address: string | undefined; 
+  signer: Signer | undefined;
+  address: string | undefined;
 }
 
 const walletContextDefaultValue = {
-    signer: undefined,
-    address: undefined,
+  signer: undefined,
+  address: undefined,
 };
 
 export const WalletContext = createContext<WalletContextProps>(walletContextDefaultValue);
@@ -18,11 +18,9 @@ export const WalletContextProvider = ({ children }: { children: React.ReactNode 
   const [web3Modal, setWeb3Modal] = useState<Web3Modal>();
   const [signer, setSigner] = useState<Signer>();
   const [address, setAddress] = useState<string>();
-  
-    
-    useEffect(() => {
-    const infuraId =
-      process.env.NEXT_PUBLIC_INFURA_ID || 'b6058e03f2cd4108ac890d3876a56d0d'  // TODO: set up own infuraID
+
+  useEffect(() => {
+    const infuraId = process.env.NEXT_PUBLIC_INFURA_ID || 'b6058e03f2cd4108ac890d3876a56d0d'; // TODO: set up own infuraID
     const providerOptions: IProviderOptions = {
       walletconnect: {
         package: WalletConnectProvider,
@@ -30,43 +28,39 @@ export const WalletContextProvider = ({ children }: { children: React.ReactNode 
           infuraId,
         },
       },
-    }
-      
-      const modal = new Web3Modal({ cacheProvider: true, providerOptions });
-      console.log(modal)
-      setWeb3Modal(modal)
-    }, [])
+    };
+
+    const modal = new Web3Modal({ cacheProvider: true, providerOptions });
+
+    setWeb3Modal(modal);
+  }, []);
 
   useEffect(() => {
-    console.log('web3Modal', web3Modal);
     const connect = async () => {
       if (!web3Modal) {
         return;
         // throw new Error('web3Modal not initialized')
-      };
-      const instance = await web3Modal.connect()
-      console.log(instance);
+      }
+      const instance = await web3Modal.connect();
+
       if (!instance) return;
       const provider = new ethers.providers.Web3Provider(instance, 'any');
-      console.log(provider);
-      const newSigner = provider.getSigner()
-      console.log('newSigner', newSigner);
+
+      const newSigner = provider.getSigner();
+
       setSigner(newSigner);
-      const { chainId: newChainId } = await provider.getNetwork();
-      console.log('newChainId', newChainId);
+
       const address = await newSigner.getAddress();
-      console.log('address', address);
+
       setAddress(address);
-      
     };
     connect();
-    
-  }, [web3Modal])
-  
+  }, [web3Modal]);
+
   const value = {
     signer,
-    address
-  }
-    
-    return <WalletContext.Provider value={value}>{children}</WalletContext.Provider>
+    address,
+  };
+
+  return <WalletContext.Provider value={value}>{children}</WalletContext.Provider>;
 };
--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,145 +1,129 @@
-import { useState, useCallback, useEffect } from "react";
-import { useWalletContext } from "../../context/WalltetContext";
-import { useXmptContext } from "../../context/XmtpContext";
-import { Conversation, Message, Stream } from '@xmtp/xmtp-js'
-import { threadId } from "worker_threads";
-
-const RecipientInputMode = {
-  InvalidEntry: 0,
-  ValidEntry: 1,
-  FindingEntry: 2,
-  Submitted: 3,
-  NotOnNetwork: 4,
-}
-
+import { useState, useCallback, useEffect } from 'react';
+import { useXmptContext } from '../../context/XmtpContext';
+import { Conversation, Message, Stream } from '@xmtp/xmtp-js';
 
 const ChatPage = () => {
-    const [addressInputValue, setAddressInputValue] = useState<string>('0xDb0b11d1281da49e950f89bD0F6B47D464d25F91');
-    const [isClientOnNetWork, setIsClientOnNetWork] = useState<boolean>(false);
-    const [recipientInputMode, setRecipientInputMode] = useState(
-    RecipientInputMode.InvalidEntry
-    )
-    const [conversation, setConversation] = useState<Conversation | null>(null);
-    const [sentMessage, setSentMessages] = useState<any[]>([])
-    const [incommingMessages, setIncomingMessages] = useState<any[]>([]);
-    const { address, signer } = useWalletContext();
-    const { client } = useXmptContext();
-    console.log('address', address);
-    console.log('signer', signer);
-    console.log('clientXXX', client);
+  const [addressInputValue, setAddressInputValue] = useState<string>('');
+  const [conversation, setConversation] = useState<Conversation | null>(null);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [incommingMessages, setIncomingMessages] = useState<any[]>([]);
 
-    const checkIfOnNetwork = useCallback(
-        async (address: string): Promise<boolean> => {
-            console.log('client', client)
-      return client?.canMessage(address) || false
+  const { client } = useXmptContext();
+  const checkIfOnNetwork = useCallback(
+    async (address: string): Promise<boolean> => {
+      return client?.canMessage(address) || false;
     },
-    [client]
-  )
+    [client],
+  );
 
-    const submitHandler: React.FormEventHandler<HTMLFormElement> = async (event) => {
-        event.preventDefault();
-        console.log('addressInputValue', addressInputValue)
-        const isInNetwork = !!addressInputValue ? await checkIfOnNetwork(addressInputValue) : false;
-        console.log('isInNetWork', isInNetwork);
-        setIsClientOnNetWork(isInNetwork);
-        if (!!isInNetwork && !!addressInputValue) {
-            setRecipientInputMode(RecipientInputMode.Submitted)
-            const newConversation = await client?.conversations.newConversation(addressInputValue)!
-            console.log('newConversation', newConversation)
-            if (!newConversation) console.error('no new conversation');
-            setConversation(newConversation)
+  const submitHandler: React.FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    const isInNetwork = !!addressInputValue
+      ? await checkIfOnNetwork(addressInputValue)
+      : false;
 
-        }
-        
-
-    }
-
-    const sendMessageHandler = () => {
-        const messageText = String(`Time stamp: ${Date.now()}`)
-        if (!conversation) {
-            alert('no conversation!');
-            return;
-        }
-        conversation.send(messageText).then((data) => {
-            console.log('send message data', data.id, data.content);
-            const newSentMessage = {id: data.id, content: data.content};
-            setSentMessages([...sentMessage, newSentMessage])
-        }).catch(error => {
-            console.error('send message error', error);
-
-        })
-    }
-
-    useEffect(() => {
-        if (!conversation) {
-            console.log('no conversation');
-            return;
-        }
-        conversation.messages().then((messages) => {
-            console.log('messages', messages);
-            setSentMessages(messages);
-        });
-
-    }, [])
-
-    useEffect(() => {
-        if (!conversation) {
-            console.log('no conversation');
-            return;
-        }
-
-        const testMess = async () => {
-            const stream = await conversation.streamMessages()
-            console.log('stream', stream);
-            for await (const msg of stream) {
-                console.log('msg', msg)
-                console.log('incomming message');
-                const incommingMess = { id: msg.id, content: msg.content}
-                setIncomingMessages([...incommingMessages, incommingMess])
-        // if (dispatchMessages) {
-        //   dispatchMessages({
-        //     peerAddress: conversation.peerAddress,
-        //     messages: [msg],
-        //   })
-        // }
-        // if (onMessageCallback) {
-        //   onMessageCallback()
-        // }
+    if (!!isInNetwork && !!addressInputValue) {
+      const newConversation = await client?.conversations.newConversation(addressInputValue)!;
+      if (!newConversation) {
+        console.error('no conversation');
+        return;
       }
-        }
-        // conversation.streamMessages().then((stream) => {
-        //     console.log('stream', stream);
-            
-            
+      setConversation(newConversation);
+    }
+  };
 
-        // })
-        testMess();
+  const sendMessageHandler = () => {
+    const messageText = String(`Time stamp: ${Date.now()}`);
+    if (!conversation) {
+      console.warn('no conversation!');
+      return;
+    }
+    conversation
+      .send(messageText)
+      .then((data) => {
+        console.log('send message data', data.id, data.content);
+      })
+      .catch((error) => {
+        console.error('send message error', error);
+      });
+  };
 
-    });
+  useEffect(() => {
+    if (!conversation) {
+      console.log('no conversation');
+      return;
+    }
 
+    const streamMessages = async () => {
+      const stream = await conversation.streamMessages();
+      for await (const msg of stream) {
+        setIncomingMessages([...incommingMessages, msg]);
+      }
+    };
 
-    return <div>Chat
-        <div>
-            0xDb0b11d1281da49e950f89bD0F6B47D464d25F91
-            <form onSubmit={submitHandler}>
-            <label>Address to chat</label>
-            <input value={addressInputValue} onChange={(event) => setAddressInputValue(event.target.value)}></input>
-            <button type="submit">Submit</button>
-            </form>
-        </div>
-        <div>Conversation
-            {conversation ? <div>Has converstaion<div>
-                <button onClick={sendMessageHandler}>Send Message</button>
-                <div style={{ display: 'flex'}}>
-                    <div style={{ padding: '5px'}}>Outgoing messages: {sentMessage.map(({ id, content }) => <div><span>{`id: ${id}`}</span><span> {content}</span></div>)}</div>
-                <div style={{ padding: '5px'}}>Incoming messages: {incommingMessages.map(({ id, content }) => <div><span>{`id: ${id}`}</span> <span>{content}</span></div>)}</div>
+    streamMessages();
+  }, [conversation]);
 
+  useEffect(() => {
+    setMessages([...incommingMessages, ...messages]);
+  }, [incommingMessages]);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      if (!conversation) {
+        console.warn('no conversation');
+        return;
+      }
+      const msgs = await conversation.messages();
+      setMessages(msgs.sort((msg1, msg2) => msg2.sent!.getTime() - msg1.sent!.getTime()));
+    };
+
+    fetchMessages();
+  }, [conversation]);
+
+  return (
+    <div>
+      Chat
+      <div>
+        {/* 0xDb0b11d1281da49e950f89bD0F6B47D464d25F91 */}
+        <form onSubmit={submitHandler}>
+          <label>Address to chat with: </label>
+          <input
+            value={addressInputValue}
+            onChange={(event) => setAddressInputValue(event.target.value)}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+        Conversation
+        {conversation ? (
+          <div>
+            Has converstaion
+            <div>
+              <button onClick={sendMessageHandler}>Send Message</button>
+              <div style={{ display: 'flex' }}>
+                <div style={{ padding: '5px' }}>
+                  Messages:{' '}
+                  {messages.map(({ id, senderAddress, recipientAddress, content, sent }) => (
+                    <div key={id} style={{ paddingBottom: '5px' }}>
+                      <div>Id: {id}</div>
+                      <div>Sender: {senderAddress}</div>
+                      <div>Recepient: {recipientAddress}</div>
+                      <div>Content: {content}</div>
+                      <div>Time sent: {sent?.toISOString()}</div>
+                    </div>
+                  ))}
                 </div>
-                
+              </div>
             </div>
-            </div> : <div>No conversation</div>}
-        </div>
-    </div>;
-}
+          </div>
+        ) : (
+          <div>No conversation</div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default ChatPage;
