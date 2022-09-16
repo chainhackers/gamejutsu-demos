2ee741284cef259e27220476b1229f016d24007e
Victor Galkin <vgalkin@itkey.com>
2022-09-16 23:06:51 +0300

    Drop get session keys. Relay on localstate

--- helpers/session_signatures.ts
+++ helpers/session_signatures.ts
@@ -1,17 +1,6 @@
 import { ethers } from "ethers";
 import arbiterContract from 'contracts/Arbiter.json';
 
-export function getSessionWallet(address:string, gameId:number): ethers.Wallet {
-    let localStorage = window.localStorage;
-    let privateStore = `${address}_${gameId}_private`;
-    let privateKey = localStorage.getItem(privateStore);
-    if (privateKey) {
-        return new ethers.Wallet(privateKey);
-    }
-    let randomWallet = ethers.Wallet.createRandom();
-    localStorage.setItem(privateStore, randomWallet.privateKey);
-    return randomWallet;
-}
 
 export async function setSessionKey(gameId:number, wallet: ethers.Wallet): Promise<void> {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -20,11 +9,17 @@ export async function setSessionKey(gameId:number, wallet: ethers.Wallet): Promi
     return await contract.setSessionKey(gameId, wallet.address, {gasLimit: gasEstimatedRedeem.mul(4)});
 }
 
-export async function getSessionKeys(gameId:number): Promise<string[]> {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const contract = new ethers.Contract(arbiterContract.address, arbiterContract.abi, provider.getSigner());
-    const gasEstimatedRedeem =  await contract.estimateGas.getSessionKeys(gameId);
-    return await contract.getSessionKeys(gameId, {gasLimit: gasEstimatedRedeem.mul(4)});
+export async function getSessionWallet(gameId:number, address:string): Promise<ethers.Wallet> {
+    let localStorage = window.localStorage;
+    let privateStore = `${address}_${gameId}_private`;
+    let privateKey = localStorage.getItem(privateStore);
+    if (privateKey) {
+        return new ethers.Wallet(privateKey);
+    }
+    let wallet = ethers.Wallet.createRandom();
+    await setSessionKey(gameId, wallet);
+    localStorage.setItem(privateStore, wallet.privateKey);
+    return wallet;
 }
 
 // All properties on a domain are optional
@@ -49,21 +44,3 @@ export async function signMove(gameMove:object, wallet: ethers.Wallet): Promise<
     let signPromise = wallet._signTypedData(domain, types, gameMove)
     return signPromise;
 }
-
-// ethereum.on('accountsChanged', this.handleAccountsChanged);
-// ethereum.on('chainChanged', this.handleChainChanged);
-
-// export function _handleChainChanged(chainId: string) {
-//     // We recommend reloading the page, unless you must do otherwise
-//     //window.location.reload();
-//     console.log("chainId", chainId);
-// };
-
-// export function _handleAccountsChanged(accounts: string[]) {
-//     console.log("accounts", accounts);
-//     if (accounts.length == 0) {
-//       let ethAccount = null;
-//     } else {
-//       let ethAccount = accounts[0];
-//     }      
-// };
