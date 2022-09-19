ac985dab952f40d5c1eb6d6bc885ae8df6c3d74a
poludnev <poludnev.roman@gmail.com>
2022-09-19 21:03:19 +0300

    fixed header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,5 @@
-import { Logo } from 'components';
 import { HeaderPropsI } from './HeaderProps';
 import styles from './Header.module.scss';
 export const Header: React.FC<HeaderPropsI> = () => {
-  return (
-    <div className={styles.container}>
-      <Logo />
-    </div>
-  );
+  return <div className={styles.container}></div>;
 };
