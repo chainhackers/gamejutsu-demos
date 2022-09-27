63e62863efe9b4badee966a3263e6c794bf24e0d
poludnev <poludnev.roman@gmail.com>
2022-09-27 19:35:25 +0300

    Added pics to games page

--- components/GameThumbnail/GameThumbnail.tsx
+++ components/GameThumbnail/GameThumbnail.tsx
@@ -1,5 +1,5 @@
-
 import { GameThumbnailPropsI } from './GameThumbnailProps';
+import Image from 'next/image';
 import styles from './GameThumbnail.module.scss';
 import Link from 'next/link';
 export const GameThumbnail: React.FC<GameThumbnailPropsI> = ({ name, image, url }) => {
@@ -7,7 +7,9 @@ export const GameThumbnail: React.FC<GameThumbnailPropsI> = ({ name, image, url 
     <div className={styles.container}>
       <Link href={url}>
         <a>
-          <div className={styles.image}>image</div>
+          <div className={styles.image}>
+            <Image src={image} layout="fill" />
+          </div>
           <div className={styles.name}>{name}</div>
         </a>
       </Link>
