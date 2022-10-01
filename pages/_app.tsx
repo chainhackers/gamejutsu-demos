f624eda2387c721e5ae1602c47445b7a7fc1a6fe
Victor Galkin <vgalkin@itkey.com>
2022-10-01 17:40:30 +0300

    Change page title and description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,7 +35,7 @@ const { chains, provider, webSocketProvider } = configureChains(
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
+  appName: 'GameJutsu App',
   chains,
 });
 
