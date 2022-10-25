610a74a8dab5731b67699605fd568e74d06ec82b
Evgeny Tsvigun <utgarda@gmail.com>
2022-10-25 20:17:01 +0300

    #5 GameConversation: new checkers contract

--- eth-sdk/eth-sdk.config.ts
+++ eth-sdk/eth-sdk.config.ts
@@ -4,7 +4,7 @@ const config: EthSdkConfig = {
   contracts: {
     polygon: {
       arbiter: '0x1f0b6DB015198028d57Eb89785Fc81637f1e72F5',
-      checkersRules: '0xDda9d756868b72Cd32bB09d5582097624a917D64',
+      checkersRules: '0x74E753609F0CF1EE23bA13Dc5B70fC7607dA0238',
       ticTacToeRules: '0xC6F81d6610A0b1bcb8cC11d50602D490b7624a96'
     },
   },
