ed3a7039d17f227f415d129eb0a8b3e5b7e24808
Evgeny Tsvigun <utgarda@gmail.com>
2022-11-14 18:52:59 +0300

    Checkers 0x4f4d4fC33827cEF701ca0b0bde6A9FAd9334cA74 #22

--- eth-sdk/eth-sdk.config.ts
+++ eth-sdk/eth-sdk.config.ts
@@ -4,7 +4,7 @@ const config: EthSdkConfig = {
   contracts: {
     polygon: {
       arbiter: '0x1f0b6DB015198028d57Eb89785Fc81637f1e72F5',
-      checkersRules: '0x3dE9DD196141afddd4ff562D44Be37Fb932479cd',
+      checkersRules: '0x4f4d4fC33827cEF701ca0b0bde6A9FAd9334cA74',
       ticTacToeRules: '0xC6F81d6610A0b1bcb8cC11d50602D490b7624a96'
     },
   },
