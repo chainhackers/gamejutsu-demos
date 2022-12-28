31d921307e0ae3aa7ea7ce6a611d770fcd5320e9
poludnev <poludnev.roman@gmail.com>
2022-12-28 18:01:13 +0300

    Changed cross to images #41

--- components/Games/Tic-Tac-Toe/NewSquare/NewSquare.tsx
+++ components/Games/Tic-Tac-Toe/NewSquare/NewSquare.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import cn from 'classnames';
 import { NewSquarePropsI } from './NewSquareProps';
 import styles from './NewSquare.module.scss';
@@ -6,8 +7,9 @@ export const NewSquare: React.FC<NewSquarePropsI> = ({ value, onClick, disputabl
     <div
       className={cn(styles.square, disputable ? styles.disputable : null)}
       onClick={onClick}
-    >
-      {value}
+    > 
+      {value === 'X' && <Image src='/images/cross.png' width={50} height={50} />}
+      {value === 'O' && <Image src='/images/naught.png' width={50} height={50} />}
     </div>
   );
 };
