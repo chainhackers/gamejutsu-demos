7ae65782956e464860569b199890d0ec312ae2ed
Roman Poludnev <51874367+poludnev@users.noreply.github.com>
2023-01-12 17:04:59 +0300

    Update checkers rules contract address

--- eth-sdk/eth-sdk.config.ts
+++ eth-sdk/eth-sdk.config.ts
@@ -4,7 +4,7 @@ const config: EthSdkConfig = {
   contracts: {
     polygon: {
       arbiter: '0x1f0b6DB015198028d57Eb89785Fc81637f1e72F5',
-      checkersRules: '0x5dA4C02a82733A7a0d4E04A51CCFF7AfFf03A7DC',
+      checkersRules: '0xC942355F56571A4C12f27fD0d183420C4b851791',
       ticTacToeRules: '0xC6F81d6610A0b1bcb8cC11d50602D490b7624a96'
     },
   },
