c01ccb0833963bdffa3a3bb3e5ff3e487ef47d5c
Roman Poludnev <51874367+poludnev@users.noreply.github.com>
2023-01-20 19:43:01 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,7 +40,7 @@ const client = new ApolloClient({
   cache,
 });
 
-const version = '1.1.1';
+const version = '1.1.2-debug';
 function MyApp({ Component, pageProps }: AppProps) {
   const props = { ...pageProps, version };
   return (
