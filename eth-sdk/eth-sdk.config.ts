8fecd606d4a44079f34ccfdae2956ad3cb8ab332
poludnev <poludnev.roman@gmail.com>
2023-01-21 21:09:23 +0300

    update checkers rules address

--- eth-sdk/eth-sdk.config.ts
+++ eth-sdk/eth-sdk.config.ts
@@ -4,7 +4,7 @@ const config: EthSdkConfig = {
   contracts: {
     polygon: {
       arbiter: '0x1f0b6DB015198028d57Eb89785Fc81637f1e72F5',
-      checkersRules: '0xC942355F56571A4C12f27fD0d183420C4b851791',
+      checkersRules: '0xf5096Da12Ba0D96Df30f813a0d4d94cc10052237',
       ticTacToeRules: '0xC6F81d6610A0b1bcb8cC11d50602D490b7624a96'
     },
   },
