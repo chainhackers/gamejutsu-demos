16eb289705d7bd31328a63138c3b3c28bffcb084
GhUserrrr <101joe.prime@gmail.com>
2023-02-09 17:41:30 +0300

    get history btn: getHistory added #140

--- pages/games/[gameType].tsx
+++ pages/games/[gameType].tsx
@@ -8,6 +8,7 @@ import {
   Disclaimer,
   DisclaimerNotice,
   GameField,
+  GetHistory,
   JoinGame,
   LeftPanel,
   RightPanel,
@@ -599,6 +600,9 @@ const Game: NextPage<IGamePageProps> = ({ gameType, version }) => {
             {gameComponent}
           </GameField>
           <RightPanel>
+          <div style={{ position: 'absolute', right: '0'}}>
+              <GetHistory history={lastMessages} messageHistory={messageHistory} gameId={gameId}/>
+            </div>
             <XMTPChatLog anyMessages={collectedMessages} isLoading={loading} />
           </RightPanel>
           {gameType === 'checkers' && <Disclaimer>{t('games.checkers.disclaimer.s1')} <strong>
