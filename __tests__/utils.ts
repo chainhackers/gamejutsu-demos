7d2fc27f6bd81b298e0c415f43e377cfbb1da881
GhUserrrr <101joe.prime@gmail.com>
2023-04-04 17:46:54 +0300

    add test of encoding move #144

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,9 +1,17 @@
 import { describe, expect, test } from '@jest/globals';
+import { CHECKERSMove } from '../components/Games/Checkers/types';
+// import { defaultAbiCoder } from 'ethers/lib/utils';
 
-import { isEvenRow } from 'helpers/utils';
+const move1: any = { move: [9, 13, true], player: 'X' };
+function isMoveCorrect(move1: any) {
+  const move: CHECKERSMove = CHECKERSMove.fromMove(move1.move, move1.player);
+  return move;
+}
 
-describe('isEvenRow', () => {
-  test('row 0 - even, row 1 - odd', () => {
-    expect(isEvenRow(0)).toBe(true);
+describe('isMoveCorrect', () => {
+  test('move from 9 to 13', () => {
+    expect(isMoveCorrect(move1).encodedMove).toBe(
+      '0x0000000000000000000000000000000000000000000000000000000000000009000000000000000000000000000000000000000000000000000000000000000d0000000000000000000000000000000000000000000000000000000000000001'
+    );
   });
 });
