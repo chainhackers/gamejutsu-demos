4f87fd867b955b3859ac6df6daed37db74f27955
GhUserrrr <101joe.prime@gmail.com>
2023-04-16 16:50:15 +0300

    add ssh-action #161

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,7 +39,7 @@ const client = new ApolloClient({
   cache,
 });
 
-const version = '1.1.21AAAAAA';
+const version = '1.1.22';
 function MyApp({ Component, pageProps }: AppProps) {
   const props = { ...pageProps, version };
   return (
