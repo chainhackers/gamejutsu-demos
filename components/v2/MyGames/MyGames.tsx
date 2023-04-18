bb3080f1d0070fc26ed8abcdb64adc3835afe893
GhUserrrr <101joe.prime@gmail.com>
2023-04-18 09:31:00 +0300

    add GameInfo #158

--- components/v2/MyGames/MyGames.tsx
+++ components/v2/MyGames/MyGames.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { MyGamesPropsI } from './MyGamesProps';
+import { GameCard } from 'components/v2/GameCard/GameCard';
+import styles from './MyGames.module.scss';
+import { GameInfo } from '../GameInfo';
 
-export const MyGames: React.FC<MyGamesPropsI> = () => {
-  return <div>MyGames</div>;
+export const MyGames: React.FC<MyGamesPropsI> = ({ games }) => {
+  return (
+    <div>
+      <div className={styles.game}>
+        <GameInfo />
+        <GameCard />
+      </div>
+    </div>
+  );
 };
--- components/v2/MyGames/MyGamesProps.ts
+++ components/v2/MyGames/MyGamesProps.ts
@@ -1,3 +1,4 @@
 export interface MyGamesPropsI {
   children?: React.ReactNode;
+  games: { name: string; image: string; url: string; description: string }[];
 }
