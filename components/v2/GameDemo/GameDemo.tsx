21e41f97e3c8eba07660439f7c0df037b597c2e5
GhUserrrr <101joe.prime@gmail.com>
2023-04-18 09:27:37 +0300

    remove unused #158

--- components/v2/GameDemo/GameDemo.tsx
+++ components/v2/GameDemo/GameDemo.tsx
@@ -1,17 +1,10 @@
-// import { GetStaticProps, NextPage } from 'next';
 import { useTranslation } from 'react-i18next';
 import { GameThumbnail } from 'components/v2/GameThumbnail';
 import styles from 'pages/v2/games/games.module.scss';
 import { GameDemoPropsI } from './GameDemoProps';
-// import { useAccount } from 'wagmi';
-
-// interface IGamesPageProps {
-//   games: { name: string; image: string; url: string; description: string }[];
-// }
 
 export const GameDemo: React.FC<GameDemoPropsI> = ({ games }) => {
   const { t } = useTranslation();
-  // const { address } = useAccount();
   return (
     <div className={styles.container}>
       <div className={styles.title}>{t('gameTypePage.title')}</div>
@@ -24,7 +17,6 @@ export const GameDemo: React.FC<GameDemoPropsI> = ({ games }) => {
               <GameThumbnail
                 key={game.name + index}
                 {...game}
-                // name={t(`gameTypePage.games.${game.name}`)}
                 name={game.name}
                 url={game.url}
                 description={game.description}
@@ -35,13 +27,3 @@ export const GameDemo: React.FC<GameDemoPropsI> = ({ games }) => {
     </div>
   );
 };
-
-// export default GameDemo;
-
-// export const getStaticProps: GetStaticProps<IGamesPageProps> = () => {
-//   return {
-//     props: {
-//       games: games,
-//     },
-//   };
-// };
