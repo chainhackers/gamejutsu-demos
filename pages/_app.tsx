23bf8b4be08b46a2829544470eb4512472ed1989
GhUserrrr <101joe.prime@gmail.com>
2023-05-05 12:53:04 +0300

    change version to 1.2.2 #154

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,7 +40,7 @@ const client = new ApolloClient({
   cache,
 });
 
-const version = '1.2.1';
+const version = '1.2.2';
 function MyApp({ Component, pageProps }: AppProps) {
   const props = { ...pageProps, version };
   return (
