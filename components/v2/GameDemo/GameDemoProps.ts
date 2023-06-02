17cd89c5bad52ed4e953645a56fd322e40d1a293
GhUserrrr <101joe.prime@gmail.com>
2023-06-02 14:50:17 +0300

    clenup #158

--- components/v2/GameDemo/GameDemoProps.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch, SetStateAction } from 'react';
-
-export interface GameDemoPropsI {
-  children?: React.ReactNode;
-  games: { name: string; image: string; url: string; description: string }[];
-  setSelectedTab: Dispatch<SetStateAction<string>>;
-}
--- components/v2/MyGames/MyGamesProps.ts
+++
@@ -1,4 +0,0 @@
-export interface MyGamesPropsI {
-  children?: React.ReactNode;
-  games: { name: string; image: string; url: string; description: string }[];
-}
