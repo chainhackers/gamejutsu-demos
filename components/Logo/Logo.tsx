2fcea5d9b8f91ac6c6aa1e55b3ca59b4ddbcdd56
Anton <hramickih@yandex.ru>
2023-07-11 16:27:32 +0500

    fix: fixed the image quality #167

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,12 +1,10 @@
 import { LogoPropsI } from "./LogoProps"
 import Image from "next/image"
 import styles from "./Logo.module.scss"
-
 export const Logo: React.FC<LogoPropsI> = ({ image }) => {
-	//TODO: проверить размеры лого относительно экранов
 	return (
 		<div className={styles.container}>
-			<Image src={image as string} width="32px" height="42px" alt="logo" quality={100} />
+			<Image src={image as string} width={32} height={42} alt="logo" />
 		</div>
 	)
 }
