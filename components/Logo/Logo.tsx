f330bac3dd0998f888271736a762c61027a2c444
Anton <hramickih@yandex.ru>
2023-07-11 16:51:12 +0500

    feat: fix build | finalized the logo by removing the 'a' tag from the link and adding it via props. #167

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,10 +1,10 @@
 import { LogoPropsI } from "./LogoProps"
 import Image from "next/image"
 import styles from "./Logo.module.scss"
-export const Logo: React.FC<LogoPropsI> = ({ image }) => {
+export const Logo: React.FC<LogoPropsI> = ({ image, href }) => {
 	return (
-		<div className={styles.container}>
+		<a href={href} className={styles.container}>
 			<Image src={image as string} width={32} height={42} alt="logo" />
-		</div>
+		</a>
 	)
 }
