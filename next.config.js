d25f8f10f9904423393ea801853e2c848e2db16e
Anton <hramickih@yandex.ru>
2023-07-11 16:59:05 +0500

    fix: change image optimization for build #167

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: false },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
