1f0a1889e316107995ca88c16746c0e81c4c1010
Anton <hramickih@yandex.ru>
2023-07-12 16:20:34 +0500

    feat: add prettier | marked this conversation as resolved #167

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  jsxBracketSameLine: true,
+  tabWidth: 2,
+  printWidth: 200,
+  singleAttributePerLine: true,
+  useTabs: false,
+}
