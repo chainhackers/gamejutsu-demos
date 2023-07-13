f06432229a1da8d71bff27460c6dcf36c4c813be
Anton <hramickih@yandex.ru>
2023-07-14 00:43:21 +0500

    feat: upd version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,7 +40,7 @@ const client = new ApolloClient({
   cache,
 });
 
-const version = 'v1.2.37AHABD';
+const version = 'v1.2.38';
 function MyApp({ Component, pageProps }: AppProps) {
   const props = { ...pageProps, version };
   return (
