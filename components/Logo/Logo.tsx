cc076ed20e892af5d03947d6f31ae09b64eaf908
Anton <hramickih@yandex.ru>
2023-07-13 23:01:58 +0500

    fix: fixed logo ref #166

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { LogoPropsI } from './LogoProps';
 import Image from 'next/image';
 import styles from './Logo.module.scss';
-export const Logo: React.FC<LogoPropsI> = ({ image, href }) => {
+export const Logo = React.forwardRef<HTMLImageElement, LogoPropsI>(({ image, href }, ref) => {
   return (
     <a href={href} className={styles.container}>
-      <Image src={image as string} width={32} height={42} alt='logo' />
+      <Image src={image as string} width={32} height={42} alt='logo'  />
     </a>
   );
-};
+});
