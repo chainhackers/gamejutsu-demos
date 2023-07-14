d0151841871fddd616d96bda3cc3e343783fc790
Anton <hramickih@yandex.ru>
2023-07-14 12:41:43 +0500

    feat: add result selection button #166

--- pages/game-result/index.tsx
+++ pages/game-result/index.tsx
@@ -3,10 +3,19 @@ import { GameResult } from 'components/GameResult';
 import { TGameResult } from 'components/GameResult/GameResultProps';
 import styles from './gameResult.module.scss';
 const GameResultPage = () => {
-  const [result, setResult] = useState<TGameResult>('win');
+  const [resultIndex, setResultIndex] = useState(0);
+  const results: TGameResult[] = ['win','lose', 'draw'];
+
+  const handleButtonClick = () => {
+    setResultIndex((prevIndex) => (prevIndex + 1) % results.length);
+  };
+  const currentResult = results[resultIndex];
   return (
     <div className={styles.container}>
-      <GameResult result={result} />
+      <button onClick={handleButtonClick} className='p-2 bg-slate-600 text-[#C89DFB] text-4xl w-[300px] rounded-3xl m-2 place-content-center'>
+        Переключить результат
+      </button>
+      <GameResult result={currentResult} />
     </div>
   );
 };
