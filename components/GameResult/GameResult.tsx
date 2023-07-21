3a5f7639cf064a368bed558c8d1fb6d8fe040c35
Anton <hramickih@yandex.ru>
2023-07-21 17:26:19 +0500

    feat: added TeamMemberBasic to the lose and draw pages #166

--- components/GameResult/GameResult.tsx
+++ components/GameResult/GameResult.tsx
@@ -79,7 +79,10 @@ export const GameResult = (props: GameResultProps) => {
           <CustomButton size='sm' color='gradient' radius='lg' text='Github' imagePosition='right' image='/images/git.svg' />
           <CustomButton size='sm' color='gradient' radius='lg' text='Publications' imagePosition='right' image='/images/publ.svg' />
         </div>
-        <h2 className={styles.titleCenter}>The dream team for your future games is here!</h2>
+        <h2 className={styles.titleCenterBottom}>The dream team for your future games is here!</h2>
+        <div className={styles.teamMemberWrapper}>
+          <div className={styles.teamMemberContainer}>{team && team.map((teamMember) => <TeamMemberBasic image={teamMember.image} name={teamMember.name} role={teamMember.role} />)}</div>
+        </div>
         <div className={styles.containerBtnColumn}>
           <CustomButton size='lg' color='gradient' radius='lg' text='Tell us about your idea' />
           <CustomButton size='lg' color='transparent' radius='sm' text='Restart Demo' imagePosition='left' image='/images/dices.svg' />
@@ -106,7 +109,10 @@ export const GameResult = (props: GameResultProps) => {
           <CustomButton size='sm' color='gradient' radius='lg' text='Github' imagePosition='right' image='/images/git.svg' />
           <CustomButton size='sm' color='gradient' radius='lg' text='Publications' imagePosition='right' image='/images/publ.svg' />
         </div>
-        <h2 className={styles.titleCenter}>The dream team for your future games is here!</h2>
+        <h2 className={styles.titleCenterBottom}>The dream team for your future games is here!</h2>
+        <div className={styles.teamMemberWrapper}>
+          <div className={styles.teamMemberContainer}>{team && team.map((teamMember) => <TeamMemberBasic image={teamMember.image} name={teamMember.name} role={teamMember.role} />)}</div>
+        </div>
         <div className={styles.containerBtnColumn}>
           <CustomButton size='lg' color='gradient' radius='lg' text='Tell us about your idea' />
           <CustomButton size='lg' color='transparent' radius='sm' text='Restart Demo' imagePosition='left' image='/images/dices.svg' />
