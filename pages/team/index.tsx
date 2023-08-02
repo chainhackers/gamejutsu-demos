bb3d38f4d6483d3dd964de0277a0d27b4b62d991
Anton <hramickih@yandex.ru>
2023-08-02 16:51:25 +0500

    fix: br h2 heading break #166

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -10,7 +10,10 @@ const Team: NextPage = () => {
       {team && team.map((teamMember) => <TeamMember {...teamMember} key={teamMember.name} />)}
       <div className={styles.margin}></div>
       <div className={styles.containerIdea}>
-        <h2 className={styles.titleCenter}>You have an idea? We have the talents!</h2>
+        <h2 className={styles.titleCenter}>
+          You have an idea? <br className={styles.break} />
+          We have the talents!
+        </h2>
         <div className={styles.customButton}>
           <CustomButton size='lg' color='transparent' radius='sm' text='Contact us' />
         </div>
