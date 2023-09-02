24d385e3659880e2b508019e0422b79043e87cfc
Anton@habdevs <a.hramickih@gmail.com>
2023-09-02 20:07:51 +0500

    upd: wagmi and rainbowkit #190

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,58 +1,53 @@
-import type {AppProps} from 'next/app';
-import Head from 'next/head';
-import {RainbowKitProvider, getDefaultWallets} from '@rainbow-me/rainbowkit';
-import {configureChains, createClient, WagmiConfig} from 'wagmi';
-import {polygon} from 'wagmi/chains';
-import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
-import {Layout} from 'components';
-import {WalletContextProvider} from 'contexts/WalltetContext';
-import {XmtpContextProvider} from 'contexts/XmtpContext';
-import 'i18n/index';
-import '@rainbow-me/rainbowkit/styles.css';
-import 'styles/globals.css';
-import XmtpProvider from '../contexts/XmtpProvider';
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { polygon } from 'wagmi/chains'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import { Layout } from 'components'
+import { WalletContextProvider } from 'contexts/WalltetContext'
+import { XmtpContextProvider } from 'contexts/XmtpContext'
+import 'i18n/index'
+import '@rainbow-me/rainbowkit/styles.css'
+import 'styles/globals.css'
+import XmtpProvider from '../contexts/XmtpProvider'
 // TODO: don't forget to bring back alchemy instead of publicProvider. @habdevs #190
 // import {alchemyProvider} from 'wagmi/providers/alchemy';
-import {GameStateContextProvider} from 'contexts/GameStateContext';
-import {publicProvider} from 'wagmi/providers/public';
+import { GameStateContextProvider } from 'contexts/GameStateContext'
+import { publicProvider } from 'wagmi/providers/public'
 
-const {chains, provider, webSocketProvider} = configureChains(
-  [polygon],
-  [publicProvider()]
+const { chains, provider, webSocketProvider } = configureChains([polygon],
+  [publicProvider()],
   // [alchemyProvider({apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!})]
-);
+)
 
-const {connectors} = getDefaultWallets({
+const { connectors } = getDefaultWallets({
   appName: 'GameJutsu App',
   chains,
-});
+  projectId: 'efa8ae30b8b72991a9630a636d6d0411',
+})
 
 const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-  webSocketProvider,
-});
+  autoConnect: true, connectors, provider, webSocketProvider,
+})
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache()
 const client = new ApolloClient({
   uri: 'https://api.thegraph.com/subgraphs/name/chainhackers/gamejutsu-subgraph',
   cache,
-});
+})
 
-const version = 'v1.2.38';
+const version = 'v1.2.38'
 
-function MyApp({Component, pageProps}: AppProps) {
-  const props = {...pageProps, version};
-  return (
-    <WagmiConfig client={wagmiClient}>
+function MyApp ({ Component, pageProps }: AppProps) {
+  const props = { ...pageProps, version }
+  return (<WagmiConfig client={wagmiClient}>
       <GameStateContextProvider>
         <ApolloProvider client={client}>
           <WalletContextProvider>
             <XmtpContextProvider>
               <RainbowKitProvider chains={chains}>
                 <XmtpProvider>
-
                   <Layout version={version}>
                     <Head>
                       <meta
@@ -62,15 +57,13 @@ function MyApp({Component, pageProps}: AppProps) {
                     </Head>
                     <Component {...props} />
                   </Layout>
-
                 </XmtpProvider>
               </RainbowKitProvider>
             </XmtpContextProvider>
           </WalletContextProvider>
         </ApolloProvider>
       </GameStateContextProvider>
-    </WagmiConfig>
-  );
+    </WagmiConfig>)
 }
 
-export default MyApp;
+export default MyApp
